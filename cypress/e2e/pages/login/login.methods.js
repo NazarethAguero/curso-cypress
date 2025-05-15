61bd5ae91fb2745adf6dcf360572737e63eda1c9
NazarethAguero <nazarethaguero3019@hotmail.com>
2025-05-15 16:22:16 -0600

    Caso de prueba "Inicio de Sesión Inválido"

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -1,3 +1,5 @@
+import { Logger } from "../../util/logger";
+import { CommonPageMethods } from "../common-page/common-page.methods";
 import { LoginElements } from "./login.elements";
 
 export class LoginMethods{
@@ -14,8 +16,15 @@ export class LoginMethods{
     }
 
     static login(username, password){
+        Logger.subStep("Insert username")
         this.insertUsername(username)
+        Logger.subStep("Insert password")
         this.insertPassword(password)
+        Logger.stepNumber('Clic on Login button')
         this.clickOnLoginButton()
+    }
+
+    static verifyWrongPasswordMessage(){
+        CommonPageMethods.verifyAlert("Wrong password")
     }
 }
